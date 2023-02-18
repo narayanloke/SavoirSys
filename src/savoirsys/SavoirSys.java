0b929298cd33d964a35297a799d96001844f9895
narayanloke <narayan.loke@pivotroots.com>
2023-02-18 21:56:47 +0530

    Added Base Commit

---
+++ src/savoirsys/SavoirSys.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package savoirsys;
+
+/**
+ *
+ * @author narayan
+ */
+public class SavoirSys {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginForm lf=new LoginForm();
+        lf.setVisible(true);
+    }
+    
+}
